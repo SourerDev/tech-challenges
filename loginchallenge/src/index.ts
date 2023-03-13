c3ab2a3203530115542fdf316976e0f2cd1365a3
jhoniernem <jhonny30ca@gmail.com>
2023-03-13 11:18:48 -0500

    Added Prisma, configured it, and created user and post models

--- loginchallenge/src/index.ts
+++ loginchallenge/src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express'
+import {PrismaClient} from "@prisma/client"
 
+const prisma = new PrismaClient()
 const app = express();
 app.use(express.json())
 
 const PORT = 3001
 
+app.post('/user', async (_req,res)=>{
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: 'Alice',
+                email: 'alice@prisma.io',
+                password: "1234"
+              },
+        })
+        res.send(user)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+    
+})
 app.get('/', (_req,res)=>{
     res.send('Hello World!') 
 })
